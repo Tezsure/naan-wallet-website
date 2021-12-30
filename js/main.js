6c05655ad322f9053410740a2b7550c7f3bffe34
Jay <jay@tezsure.com>
2021-12-30 18:07:43 +0530

    Fixed margin and padding

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 const noOfChar = 60;
-const content = document.querySelectorAll('.para');
+const content = document.querySelectorAll(".para");
 
 content.forEach((content) => {
   if (content.textContent.length < noOfChar) {
-    content.nextElementSibling.getElementsByClassName.display = 'none';
+    content.nextElementSibling.getElementsByClassName.display = "none";
   } else {
     const displayText = content.textContent.slice(0, noOfChar);
     const moretext = content.textContent.slice(noOfChar);
@@ -13,44 +13,43 @@ content.forEach((content) => {
 });
 function readMore(btn, e) {
   e.stopPropagation();
-  console.log('read more');
   const post = btn.parentElement;
-  post.querySelector('.dots').classList.toggle('hide');
-  post.querySelector('.more').classList.toggle('hide');
-  btn.textContent == 'Read More'
-    ? (btn.textContent = 'Read Less')
-    : (btn.textContent = 'Read More');
+  post.querySelector(".dots").classList.toggle("hide");
+  post.querySelector(".more").classList.toggle("hide");
+  btn.textContent == "Read More"
+    ? (btn.textContent = "Read Less")
+    : (btn.textContent = "Read More");
 }
-function test() {
-  console.log('testing');
+function redirect(link) {
+  window.location.href = link;
 }
-const mobileMenuBtn = document.getElementById('mobile-menu-btn');
-const mobileMenu = document.getElementById('mobile-side-menu');
-const closeMobileMenu = document.getElementById('close-drawer-menu');
-const backdrop = document.getElementById('backdrop');
-const dropdownMenuLink = document.getElementById('menu-dropdown-link');
-const dropdownMenuWrapper = document.getElementById('dropdown-menu-wrapper');
-const websiteLoader = document.getElementById('website-loader');
+const mobileMenuBtn = document.getElementById("mobile-menu-btn");
+const mobileMenu = document.getElementById("mobile-side-menu");
+const closeMobileMenu = document.getElementById("close-drawer-menu");
+const backdrop = document.getElementById("backdrop");
+const dropdownMenuLink = document.getElementById("menu-dropdown-link");
+const dropdownMenuWrapper = document.getElementById("dropdown-menu-wrapper");
+const websiteLoader = document.getElementById("website-loader");
 
-mobileMenuBtn.addEventListener('click', () => {
-  mobileMenu.classList.add('show-drawer');
-  backdrop.classList.remove('hide');
+mobileMenuBtn.addEventListener("click", () => {
+  mobileMenu.classList.add("show-drawer");
+  backdrop.classList.remove("hide");
 });
-closeMobileMenu.addEventListener('click', () => {
-  mobileMenu.classList.remove('show-drawer');
-  backdrop.classList.add('hide');
+closeMobileMenu.addEventListener("click", () => {
+  mobileMenu.classList.remove("show-drawer");
+  backdrop.classList.add("hide");
 });
-dropdownMenuLink.addEventListener('mouseenter', () => {
-  dropdownMenuWrapper.classList.remove('hide');
+dropdownMenuLink.addEventListener("mouseenter", () => {
+  dropdownMenuWrapper.classList.remove("hide");
 });
-dropdownMenuWrapper.addEventListener('mouseleave', () => {
-  dropdownMenuWrapper.classList.add('hide');
+dropdownMenuWrapper.addEventListener("mouseleave", () => {
+  dropdownMenuWrapper.classList.add("hide");
 });
-dropdownMenuLink.addEventListener('mouseleave', () => {
-  dropdownMenuWrapper.classList.add('hide');
+dropdownMenuLink.addEventListener("mouseleave", () => {
+  dropdownMenuWrapper.classList.add("hide");
 });
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener("DOMContentLoaded", () => {
   setTimeout(() => {
-    websiteLoader.classList.add('hide');
+    websiteLoader.classList.add("hide");
   }, 3000);
 });
