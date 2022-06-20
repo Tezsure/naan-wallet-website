fa0bedcec8c1f9f8bdb0a6fc531844342e80a4b9
bernd.tez <berndoostrum@gmail.com>
2022-06-21 01:49:17 +0200

    Updated dapp browser

--- js/main.js
+++ js/main.js
@@ -2,26 +2,26 @@ const noOfChar = 60;
 const content = document.querySelectorAll(".para");
 
 content.forEach((content) => {
-  if (content.textContent.length < noOfChar) {
-    content.nextElementSibling.getElementsByClassName.display = "none";
-  } else {
-    const displayText = content.textContent.slice(0, noOfChar);
-    const moretext = content.textContent.slice(noOfChar);
+    if (content.textContent.length < noOfChar) {
+        content.nextElementSibling.getElementsByClassName.display = "none";
+    } else {
+        const displayText = content.textContent.slice(0, noOfChar);
+        const moretext = content.textContent.slice(noOfChar);
 
-    content.innerHTML = `${displayText}<span class="dots">...</span><span class="hide more">${moretext}</span>`;
-  }
+        content.innerHTML = `${displayText}<span class="dots">...</span><span class="hide more">${moretext}</span>`;
+    }
 });
 function readMore(btn, e) {
-  e.stopPropagation();
-  const post = btn.parentElement;
-  post.querySelector(".dots").classList.toggle("hide");
-  post.querySelector(".more").classList.toggle("hide");
-  btn.textContent == "Read More"
-    ? (btn.textContent = "Read Less")
-    : (btn.textContent = "Read More");
+    e.stopPropagation();
+    const post = btn.parentElement;
+    post.querySelector(".dots").classList.toggle("hide");
+    post.querySelector(".more").classList.toggle("hide");
+    btn.textContent == "Read More"
+        ? (btn.textContent = "Read Less")
+        : (btn.textContent = "Read More");
 }
 function redirect(link) {
-  window.location.href = link;
+    window.location.href = link;
 }
 const mobileMenuBtn = document.getElementById("mobile-menu-btn");
 const mobileMenu = document.getElementById("mobile-side-menu");
@@ -32,24 +32,24 @@ const dropdownMenuWrapper = document.getElementById("dropdown-menu-wrapper");
 const websiteLoader = document.getElementById("website-loader");
 
 mobileMenuBtn.addEventListener("click", () => {
-  mobileMenu.classList.add("show-drawer");
-  backdrop.classList.remove("hide");
+    mobileMenu.classList.add("show-drawer");
+    backdrop.classList.remove("hide");
 });
 closeMobileMenu.addEventListener("click", () => {
-  mobileMenu.classList.remove("show-drawer");
-  backdrop.classList.add("hide");
+    mobileMenu.classList.remove("show-drawer");
+    backdrop.classList.add("hide");
 });
 dropdownMenuLink.addEventListener("mouseenter", () => {
-  dropdownMenuWrapper.classList.remove("hide");
+    dropdownMenuWrapper.classList.remove("hide");
 });
 dropdownMenuWrapper.addEventListener("mouseleave", () => {
-  dropdownMenuWrapper.classList.add("hide");
+    dropdownMenuWrapper.classList.add("hide");
 });
 dropdownMenuLink.addEventListener("mouseleave", () => {
-  dropdownMenuWrapper.classList.add("hide");
+    dropdownMenuWrapper.classList.add("hide");
 });
 window.addEventListener("DOMContentLoaded", () => {
-  setTimeout(() => {
-    websiteLoader.classList.add("hide");
-  }, 3000);
+    setTimeout(() => {
+        websiteLoader.classList.add("hide");
+    }, 3000);
 });
